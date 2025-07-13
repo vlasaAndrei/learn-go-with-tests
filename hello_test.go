e0ee74bc4232b0d59254102594f53d77fd818dac
vlasaAndrei <vlasa.andrei@agilefreaks.com>
2025-07-13 18:01:12 +0300

    Day1

---
+++ hello_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("default to Hello, World if no argument is provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
