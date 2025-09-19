127eac4b4d5fc2b520bce027173599c6b6e16812
vlasaAndrei <vlasa.andrei@agilefreaks.com>
2025-09-20 02:39:01 +0300

    Started the work on context section

---
+++ context/server.go
@@ -0,0 +1,18 @@
+package context
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch() string
+	Cancel()
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		store.Cancel()
+		fmt.Fprintf(w, "%s", store.Fetch())
+	}
+}
