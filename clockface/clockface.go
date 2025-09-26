4dfd0986b213286ff0c2daa98846bc1a0527330d
vlasaAndrei <vlasa.andrei@agilefreaks.com>
2025-09-26 09:52:24 +0300

    Started off the XML Parsing section

--- clockface/clockface.go
+++ clockface/clockface.go
@@ -10,13 +10,19 @@ type Point struct {
 	Y float64
 }
 
+const (
+	secondHandLength = 90
+	clockCentreX     = 150
+	clockCentreY     = 150
+)
+
 func SecondHand(t time.Time) Point {
-	return Point{150, 60}
-}
+	p := secondHandPoint(t)
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
+	p = Point{p.X, -p.Y}
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY}
 
-func secondsInRadians(t time.Time) float64 {
-	return math.Pi / (30 / float64(t.Second()))
-
+	return p
 }
 
 func secondHandPoint(t time.Time) Point {
@@ -25,4 +31,9 @@ func secondHandPoint(t time.Time) Point {
 	y := math.Cos(angle)
 
 	return Point{x, y}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return math.Pi / (30 / float64(t.Second()))
+
 }
--- clockface/clockface_acceptance_test.go
+++ clockface/clockface_acceptance_test.go
@@ -1,23 +1,56 @@
 package clockface_test
 
-// func TestSecondHandAtMidnight(t *testing.T) {
-// 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+import (
+	"encoding/xml"
+	"testing"
+	"time"
 
-// 	want := clockface.Point{X: 150, Y: 150 - 90}
-// 	got := clockface.SecondHand(tm)
+	"example.com/hello/clockface"
+)
 
-// 	if got != want {
-// 		t.Errorf("Got %v, wanted %v", got, want)
-// 	}
-// }
+type Svg struct {
+	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  struct {
+		Text  string `xml:",chardata"`
+		Cx    string `xml:"cx,attr"`
+		Cy    string `xml:"cy,attr"`
+		R     string `xml:"r,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"circle"`
+	Line []struct {
+		Text  string `xml:",chardata"`
+		X1    string `xml:"x1,attr"`
+		Y1    string `xml:"y1,attr"`
+		X2    string `xml:"x2,attr"`
+		Y2    string `xml:"y2,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"line"`
+}
 
-// func TestSecondHandAt30Seconds(t *testing.T) {
-// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-// 	want := clockface.Point{X: 150, Y: 150 + 90}
-// 	got := clockface.SecondHand(tm)
+	want := clockface.Point{X: 150, Y: 150 - 90}
+	got := clockface.SecondHand(tm)
 
-// 	if got != want {
-// 		t.Errorf("Got %v, wanted %v", got, want)
-// 	}
-// }
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
+
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+	want := clockface.Point{X: 150, Y: 150 + 90}
+	got := clockface.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
