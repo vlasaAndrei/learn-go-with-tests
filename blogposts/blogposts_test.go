605c51cc3113fab870b64ef29a9757b2f7a19482
vlasaAndrei <vlasa.andrei@agilefreaks.com>
2025-09-29 03:28:22 +0300

    Pushing blogposts package to be used in the tests

---
+++ blogposts/blogposts_test.go
@@ -0,0 +1,42 @@
+package blogposts_test
+
+import (
+	"errors"
+	"io/fs"
+	"reflect"
+	"testing"
+	"testing/fstest"
+
+	blogposts "github.com/vlasaAndrei/go-tdd/reading-files"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("hi")},
+		"hello-world2.md": {Data: []byte("hola")},
+	}
+
+	posts, err := blogposts.NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+
+	got := posts[0]
+	want := blogposts.Post{Title: "Post 1"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
+
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, I always fail")
+}
---
+++ blogposts/go.mod
@@ -0,0 +1,3 @@
+module github.com/vlasaAndrei/blogposts
+
+go 1.24.5
