e5182373391a977c048e35d0ccee1a377750dfc9
vlasaAndrei <vlasa.andrei@agilefreaks.com>
2025-10-05 00:05:29 +0300

    Split the templates

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -18,13 +18,24 @@ type Post struct {
 	Tags        []string
 }
 
-func Render(w io.Writer, p Post) error {
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
 	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	if err := templ.Execute(w, p); err != nil {
+	return &PostRenderer{templ: templ}, nil
+
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
 		return err
 	}
 
--- blogrenderer/blogrenderer_test.go
+++ blogrenderer/blogrenderer_test.go
@@ -2,6 +2,7 @@ package blogrenderer_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	approvals "github.com/approvals/go-approval-tests"
@@ -18,13 +19,40 @@ func TestRender(t *testing.T) {
 		}
 	)
 
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
 
-		if err := blogrenderer.Render(&buf, aPost); err != nil {
+		if err := postRenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
 		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for b.Loop() {
+		postRenderer.Render(io.Discard, aPost)
+	}
 }
