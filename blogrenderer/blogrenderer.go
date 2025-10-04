11da4836960083b886541f878c4b21d7e7cd5de8
vlasaAndrei <vlasa.andrei@agilefreaks.com>
2025-10-04 22:58:46 +0300

    Used approvals package to create a snapshot of the blog.gohtml

---
+++ blogrenderer/blogrenderer.go
@@ -0,0 +1,32 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+)
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type Post struct {
+	Title       string
+	Body        string
+	Description string
+	Tags        []string
+}
+
+func Render(w io.Writer, p Post) error {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ blogrenderer/blogrenderer_test.go
@@ -0,0 +1,30 @@
+package blogrenderer_test
+
+import (
+	"bytes"
+	"testing"
+
+	approvals "github.com/approvals/go-approval-tests"
+	"github.com/vlasaAndrei/learn-go-with-tests/blogrenderer"
+)
+
+func TestRender(t *testing.T) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	t.Run("it converts a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+
+		if err := blogrenderer.Render(&buf, aPost); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
+	})
+}
