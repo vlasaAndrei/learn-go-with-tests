3210cc7b359d1c3afcf50b37c2c9722167d63069
vlasaAndrei <vlasa.andrei@agilefreaks.com>
2025-10-05 20:14:27 +0300

    Added http_server

---
+++ http_server/go.mod
@@ -0,0 +1,3 @@
+module github.com/vlasaAndrei/learn-go-with-tests/http_server
+
+go 1.24.5
---
+++ http_server/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+// server.go
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "players/")
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	}
+
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
+}
